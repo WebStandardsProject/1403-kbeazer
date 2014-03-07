b955d9e516de9baae5841ae7781fd309fe28d043
kbeazer <kbeazer@fullsail.edu>
2014-03-07 13:44:27 -0500

    js file added to repo

---
+++ Project/js/init.js
@@ -0,0 +1,15 @@
+console.log('im working baby!');
+$(document).ready(function(){
+	// $('a').hover(function(){
+
+	// 	$('ul li').css('backgroundColor', '#666');
+	// 	$(this).css('color', '#efefef');
+
+	// },function(){
+	// 	// $('ul li').css('backgroundColor', '#efefef');
+	// 	$(this).css('color', '#577377');	 
+	// 	})
+	// });
+
+
+// });
